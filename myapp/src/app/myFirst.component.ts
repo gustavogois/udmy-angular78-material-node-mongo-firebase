018d85a5a04aad26e2efb21ea64d18dde0382987
Gustavo Gois <gustavo.gois@gmail.com>
2020-07-04 14:01:10 +0100

    #4 com styles

--- myapp/src/app/myFirst.component.ts
+++ myapp/src/app/myFirst.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'myfirst',
-  templateUrl: './myFirst.component.html'
+  templateUrl: './myFirst.component.html',
+  styles: [`
+    .box1 {
+      width: 200px;
+      height: 100px;
+      background: chartreuse;
+      box-shadow: 5px 10px #CCCCCC;
+    }
+  `]
 })
 
 export class MyFirstComponent {
