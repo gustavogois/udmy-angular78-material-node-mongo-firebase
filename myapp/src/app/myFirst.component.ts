b73a15ff0ad343830ad0a308a64a0c73de83cce2
Gustavo Gois <gustavo.gois@gmail.com>
2020-07-04 14:04:42 +0100

    #4 Utilizando stylesUrl

--- myapp/src/app/myFirst.component.ts
+++ myapp/src/app/myFirst.component.ts
@@ -3,14 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'myfirst',
   templateUrl: './myFirst.component.html',
-  styles: [`
-    .box1 {
-      width: 200px;
-      height: 100px;
-      background: chartreuse;
-      box-shadow: 5px 10px #CCCCCC;
-    }
-  `]
+  styleUrls: ['./myFirst.component.css']
 })
 
 export class MyFirstComponent {
