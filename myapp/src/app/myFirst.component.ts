b5c12d6db2cbca4fb8ef189de975110b0f3bff63
Gustavo Gois <gustavo.gois@gmail.com>
2020-07-04 14:12:52 +0100

    #5 como uma diretiva

--- myapp/src/app/myFirst.component.ts
+++ myapp/src/app/myFirst.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'myfirst',
+  selector: '[myfirst]',
   templateUrl: './myFirst.component.html',
   styleUrls: ['./myFirst.component.css']
 })
