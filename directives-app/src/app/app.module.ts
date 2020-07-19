592f056555b0cca360dcb957f161ff50f5d7dbc8
Gustavo Gois <gustavo.gois@gmail.com>
2020-07-19 20:12:26 +0100

    ngSwitch

--- directives-app/src/app/app.module.ts
+++ directives-app/src/app/app.module.ts
@@ -4,13 +4,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { NgForComponent } from './ng-for/ng-for.component';
-import { NgStyleComponent } from './ng-style/ng-style.component';
-import { NgClassComponent } from './ng-class/ng-class.component';
-import { NgIfComponent } from './ng-if/ng-if.component';
-import { NgForFormComponent } from './ng-for-form/ng-for-form.component';
-
 import { MatButtonModule } from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
 import { MatCardModule } from '@angular/material/card';
@@ -20,6 +13,15 @@ import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+import { NgSwitchComponent } from './ng-switch/ng-switch.component';
+import { AppComponent } from './app.component';
+import { NgForComponent } from './ng-for/ng-for.component';
+import { NgStyleComponent } from './ng-style/ng-style.component';
+import { NgClassComponent } from './ng-class/ng-class.component';
+import { NgIfComponent } from './ng-if/ng-if.component';
+import { NgForFormComponent } from './ng-for-form/ng-for-form.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,8 @@ import { MatSelectModule } from '@angular/material/select';
     NgClassComponent,
     NgIfComponent,
     NgForComponent,
-    NgForFormComponent
+    NgForFormComponent,
+    NgSwitchComponent
   ],
   imports: [
     BrowserModule,
@@ -42,7 +45,8 @@ import { MatSelectModule } from '@angular/material/select';
     MatInputModule,
     MatListModule,
     MatRadioModule,
-    MatSelectModule
+    MatSelectModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
