655639b5afad67cd4e43caa060fb2a0b57a5268a
Gustavo Gois <gustavo.gois@gmail.com>
2020-07-21 21:53:54 +0100

    Comunicação entre componentes: EventEmitter

--- communication-app/src/app/app.module.ts
+++ communication-app/src/app/app.module.ts
@@ -5,16 +5,27 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InputBindingComponent } from './input-binding/input-binding.component';
 import { ClientComponent } from './input-binding/client/client.component';
+import { EventComponent } from './event/event.component';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { ChildItemComponent } from './event/child-item/child-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputBindingComponent,
-    ClientComponent
+    ClientComponent,
+    EventComponent,
+    ChildItemComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
